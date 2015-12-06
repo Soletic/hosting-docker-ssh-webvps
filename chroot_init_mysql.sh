b5d9313b2465f3f7f0a0abfe7763f752a3d2c3fe
Laurent Chedanne <laurent@chedanne.pro>
2015-12-06 09:21:56 +0100

    - Add php alias - New system to config mysql connection with IP of mysql container

--- chroot_init_mysql.sh
+++ chroot_init_mysql.sh
@@ -17,14 +17,18 @@ case "$1" in
 					port="$2"
 					shift # past argument
 					;;
+				-ip|--mysql-host-ip)
+					host_ip="$2"
+					shift # past argument
+					;;
 				*)
 					# unknown option
 					shift
 					;;
 			esac
 		done
-		if [ -z "$user" ] || [ -z "$port" ]; then
-			>&2 echo "[conf] argument missing. Usage : $0 conf --port|-P <mysqlport> --user|-u <user>"
+		if [ -z "$user" ] || [ -z "$port" ] || [ -z "$host_ip" ]; then
+			>&2 echo "[conf] argument missing. Usage : $0 conf --port|-P <mysqlport> --user|-u <user> -ip|--mysql-host-ip <host_ip>"
 			exit 1
 		fi
 		chroot_dir=${CHROOT_INSTALL_DIR}/$user
@@ -33,10 +37,19 @@ case "$1" in
 			exit 1
 		fi
 
-		host_ip=$(/sbin/ip route|awk '/default/ { print $3 }')
-		sed -ri -e "s/^port=$/port=$port/" $chroot_dir/etc/mysql/my.cnf
-		sed -ri -e "s/^mysql.default_port.*/mysql.default_port=$port/" \
+		# PHP mysql client conf
+		sed -ri -e "s/^mysql.default_host.*/mysql.default_host=$host_ip/" \
+			-e "s/^mysqli.default_host.*/mysqli.default_host=$host_ip/" \
+			-e "s/^mysql.default_port.*/mysql.default_port=$port/" \
 			-e "s/^mysqli.default_port.*/mysqli.default_port=$port/" $chroot_dir/etc/php5/cli/php.ini
+
+		# Mysql client conf
+		sed -ri -e "s/^host=.*/host=$host_ip/" \
+			-e "s/^port=.*/port=$port/" $chroot_dir/etc/mysql/my.cnf
+		
+		;;
+	setip)
+		
 		;;
 	*)
 		>&2 echo "Command $1 not found. Usage : $0 <command> <options>"
--- setup.sh
+++ setup.sh
@@ -2,14 +2,7 @@
 
 echo "Setup..."
 
-host_ip=$(/sbin/ip route|awk '/default/ { print $3 }')
-
 # php conf
 sed -ri -e "s/^memory_limit.*/memory_limit = ${PHP_MEMORY_LIMIT}/" \
     -e "s/^;date\.timezone.*/date.timezone = $(echo ${PHP_TIME_ZONE} | sed -e 's/\//\\\//g')/" /etc/php5/cli/php.ini
 sed -ri -e "s/^date\.timezone.*/date.timezone = $(echo ${PHP_TIME_ZONE} | sed -e 's/\//\\\//g')/" /etc/php5/cli/php.ini
-sed -ri -e "s/^mysql.default_host.*/mysql.default_host=$host_ip/" \
-	-e "s/^mysqli.default_host.*/mysqli.default_host=$host_ip/" /etc/php5/cli/php.ini
-
-# mysql conf
-sed -ri -e "s/^host=$/host=$host_ip/" /etc/mysql/my.cnf
